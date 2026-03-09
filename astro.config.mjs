f01a10b2f20e130f8ed56a30d09a606376395d11
Jonathan-Chak <133053615+Jonathan-Chak@users.noreply.github.com>
2026-03-09 07:25:56 -0400

    added netlify adapter
    i have no idea what that means

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   // Example: Use the function syntax to customize based on command
   server: ({ command }) => ({ port: command === 'dev' ? 5000 : 4000 }),
+
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+
+  adapter: netlify()
 });
